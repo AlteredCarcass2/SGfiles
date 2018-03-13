942ec6f0661103cefd2622ab9c294f2e007c9eba
glas <glas@PC546.SPSEJECNANET.CZ>
2018-03-13 11:53:35 +0100

    First commit

---
+++ SGFileReader/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author glas
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
